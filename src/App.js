cd829fbbd7bbf51df86808899865a0747215a54a
GildedBaboon <vedereysahaj1@gmail.com>
2020-09-15 21:36:36 -0400

    cleaned create-react and app and updated the index.html

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
 
-  console.log("hello what's up")
-  var x = "hello"
-  //random js stuff 
-
   return (
     <div className="App"> 
-      <h1>{x}</h1>
     </div>
   );
 }
