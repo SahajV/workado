9aa0b20cc712e77163ca6607053a26ee7c4d72c3
Khavab <2021snomani@tjhsst.edu>
2020-09-19 11:18:07 -0400

    changes to form

--- src/components/ClassForm.js
+++ src/components/ClassForm.js
@@ -1,6 +1,54 @@
 import React from 'react'
+import useUser from "../_hooks/useUser";
+
+const BootstrapInput = withStyles((theme) => ({
+    root: {
+      'label + &': {
+        marginTop: theme.spacing(3),
+      },
+    },
+    input: {
+      borderRadius: 4,
+      position: 'relative',
+      backgroundColor: theme.palette.background.paper,
+      border: '1px solid #ced4da',
+      fontSize: 16,
+      padding: '10px 26px 10px 12px',
+      transition: theme.transitions.create(['border-color', 'box-shadow']),
+      // Use the system font instead of the default Roboto font.
+      fontFamily: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+      ].join(','),
+      '&:focus': {
+        borderRadius: 4,
+        borderColor: '#80bdff',
+        boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
+      },
+    },
+  }))(InputBase);
+
+const useStyles = makeStyles((theme) => ({
+    margin: {
+      margin: theme.spacing(1),
+    },
+}));
 
 function ClassForm() {
+    const classes = useStyles();
+    const [age, setAge] = React.useState('');
+    const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
 return( 
     <form>
         <input type="text">Instructor</input>
