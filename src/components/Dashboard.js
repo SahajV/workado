fc42e18019d56b4d2d1da9b89717e4a0556fde0f
GildedBaboon <vedereysahaj1@gmail.com>
2020-09-19 13:49:48 -0400

    fixed merging 2

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,23 +1,17 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
 import useUser from "../_hooks/useUser";
-<<<<<<< HEAD
 import RsModal from "./RsModal"
-=======
 import NavbarInstance from "./NavbarInstance";
 import ClassModal from "./ClassModal"
->>>>>>> f93c006cfff45a0ab9150a78287d33f7ac80da50
 
 export default function Dashboard() {
   const { isLoggedIn, userState } = useUser();
   return isLoggedIn() ? (
     <div>
-<<<<<<< HEAD
         <RsModal />
-=======
         <NavbarInstance />
         <ClassModal />
->>>>>>> f93c006cfff45a0ab9150a78287d33f7ac80da50
     </div>
   ) : (
       <div>
