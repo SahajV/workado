dac228f9f5cd3f3848befc97e00648d1df77cddb
Khavab <2021snomani@tjhsst.edu>
2020-09-19 11:51:44 -0400

    ToDo Form made

---
+++ src/components/ToDoForm.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import NativeSelect from '@material-ui/core/NativeSelect';
+import InputBase from '@material-ui/core/InputBase';
+import useUser from "../_hooks/useUser";
+
+const BootstrapInput = withStyles((theme) => ({
+    root: {
+        'label + &': {
+            marginTop: theme.spacing(3),
+        },
+    },
+    input: {
+        borderRadius: 4,
+        position: 'relative',
+        backgroundColor: theme.palette.background.paper,
+        border: '1px solid #ced4da',
+        fontSize: 16,
+        padding: '10px 26px 10px 12px',
+        transition: theme.transitions.create(['border-color', 'box-shadow']),
+        // Use the system font instead of the default Roboto font.
+        fontFamily: [
+            '-apple-system',
+            'BlinkMacSystemFont',
+            '"Segoe UI"',
+            'Roboto',
+            '"Helvetica Neue"',
+            'Arial',
+            'sans-serif',
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+        ].join(','),
+        '&:focus': {
+            borderRadius: 4,
+            borderColor: '#80bdff',
+            boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
+        },
+    },
+}))(InputBase);
+
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+    },
+    selectEmpty: {
+        marginTop: theme.spacing(2),
+    },
+}));
+
+export default function SimpleSelect() {
+    const classes = useStyles();
+    const [cla, setClass] = React.useState('');
+
+    const handleChange = (event) => {
+        setClass(event.target.value);
+    };
+
+    function ClassForm() {
+        const classes = useStyles();
+        const [age, setAge] = React.useState('');
+        const handleChange = (event) => {
+            setAge(event.target.value);
+        };
+
+        return (
+            <div>
+                <FormControl className={classes.margin}>
+                    <InputLabel htmlFor="TaskName">Task</InputLabel>
+                    <BootstrapInput id="TaskName" />
+                </FormControl>
+                <FormControl className={classes.formControl}>
+                    <InputLabel id="className">Age</InputLabel>
+                    <Select
+                        labelId="className"
+                        id="name"
+                        value={cla}
+                        onChange={handleChange}
+                    >
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                    </Select>
+                </FormControl>
+            </div>
+        );
+    }
+
+    export default ClassForm;
